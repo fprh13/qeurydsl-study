66db6d6e90cbc711efdacb97a38c0238bc68f59f
fprh13 <fprh13@gamil.com>
2023-12-03 23:50:13 +0900

    init: querydsl 세팅
    의존성 추가 및 빌드 확인
    querydsl 테스트 성공 확인

---
+++ src/main/java/com/example/querydsl/domain/Hello.java
@@ -0,0 +1,22 @@
+package com.example.querydsl.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
