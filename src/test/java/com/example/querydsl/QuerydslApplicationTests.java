8682103af25864c1f64b8d7496d1edef1c18b8f8
fprh13 <fprh13@gmail.com>
2023-12-04 19:08:23 +0900

    feat: entity 추가
    member,team

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -7,12 +7,14 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
